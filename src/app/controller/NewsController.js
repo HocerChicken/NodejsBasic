74a2224672af75ca7c2e82a3405fc327af9ed09e
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-01-03 11:05:53 +0700

    install prettier, lint-staged, husky

--- src/app/controller/NewsController.js
+++ src/app/controller/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
-    //[GET] /news
-    index(req, res) {
-        res.render("news");
-    }
+  //[GET] /news
+  index(req, res) {
+    res.render('news');
+  }
 
-    //[GET] /news/:slug
-    show(req, res) {
-        res.send("Hello Slug in news");
-    }
+  //[GET] /news/:slug
+  show(req, res) {
+    res.send('Hello Slug in news');
+  }
 }
 
 module.exports = new NewsController();
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const siteController = require("../app/controller/SiteController");
+const express = require('express');
+const siteController = require('../app/controller/SiteController');
 const router = express.Router();
 
-router.use("/search", siteController.search);
-router.use("/", siteController.index);
+router.use('/search', siteController.search);
+router.use('/', siteController.index);
 
 module.exports = router;
